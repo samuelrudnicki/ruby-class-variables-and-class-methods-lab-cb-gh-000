0c35fe0f63b4d2043bf0aadea0369cfa1ad9b8a7
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-09 19:26:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,5 +37,13 @@ class Song
   end
 
   def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if !artist_count[artist]
+        artist_count[artist] = 0
+      end
+      artist_count[artist] += 1
+    end
+    genre_count
   end
 end
