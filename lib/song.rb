3bd6de167ceee2b58e708a2b79d5a9b4c23ab297
Samuel Rudnicki <samuel.rudnicki@gmail.com>
2020-06-09 19:17:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,8 +9,8 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    @@artists << artist if !@@artists.include?(artist)
-    @@genres << genre if !@@genres.include?(genre)
+    @@artists << artist
+    @@genres << genre
   end
 
   def self.count
